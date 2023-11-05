06b08fe78950caae2b357698325150c9e297f704
avareno <miguelcabral798@gmail.com>
2023-11-05 22:13:03 +0000

    Final touches

---
+++ cmake-build-debug/CMakeFiles/projeto.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Change.cpp" "CMakeFiles/projeto.dir/Change.cpp.o" "gcc" "CMakeFiles/projeto.dir/Change.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Change_Class.cpp" "CMakeFiles/projeto.dir/Change_Class.cpp.o" "gcc" "CMakeFiles/projeto.dir/Change_Class.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Change_UC.cpp" "CMakeFiles/projeto.dir/Change_UC.cpp.o" "gcc" "CMakeFiles/projeto.dir/Change_UC.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Class.cpp" "CMakeFiles/projeto.dir/Class.cpp.o" "gcc" "CMakeFiles/projeto.dir/Class.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Class_per_uc.cpp" "CMakeFiles/projeto.dir/Class_per_uc.cpp.o" "gcc" "CMakeFiles/projeto.dir/Class_per_uc.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Menu.cpp" "CMakeFiles/projeto.dir/Menu.cpp.o" "gcc" "CMakeFiles/projeto.dir/Menu.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Quit.cpp" "CMakeFiles/projeto.dir/Quit.cpp.o" "gcc" "CMakeFiles/projeto.dir/Quit.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/SearchStudents.cpp" "CMakeFiles/projeto.dir/SearchStudents.cpp.o" "gcc" "CMakeFiles/projeto.dir/SearchStudents.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/SearchUC.cpp" "CMakeFiles/projeto.dir/SearchUC.cpp.o" "gcc" "CMakeFiles/projeto.dir/SearchUC.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/Student_class.cpp" "CMakeFiles/projeto.dir/Student_class.cpp.o" "gcc" "CMakeFiles/projeto.dir/Student_class.cpp.o.d"
+  "/home/avareno/CLionProjects/aulas/Project/Project_Code/main.cpp" "CMakeFiles/projeto.dir/main.cpp.o" "gcc" "CMakeFiles/projeto.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/projeto.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projeto.dir/Change.cpp.o"
+  "CMakeFiles/projeto.dir/Change.cpp.o.d"
+  "CMakeFiles/projeto.dir/Change_Class.cpp.o"
+  "CMakeFiles/projeto.dir/Change_Class.cpp.o.d"
+  "CMakeFiles/projeto.dir/Change_UC.cpp.o"
+  "CMakeFiles/projeto.dir/Change_UC.cpp.o.d"
+  "CMakeFiles/projeto.dir/Class.cpp.o"
+  "CMakeFiles/projeto.dir/Class.cpp.o.d"
+  "CMakeFiles/projeto.dir/Class_per_uc.cpp.o"
+  "CMakeFiles/projeto.dir/Class_per_uc.cpp.o.d"
+  "CMakeFiles/projeto.dir/Menu.cpp.o"
+  "CMakeFiles/projeto.dir/Menu.cpp.o.d"
+  "CMakeFiles/projeto.dir/Quit.cpp.o"
+  "CMakeFiles/projeto.dir/Quit.cpp.o.d"
+  "CMakeFiles/projeto.dir/SearchStudents.cpp.o"
+  "CMakeFiles/projeto.dir/SearchStudents.cpp.o.d"
+  "CMakeFiles/projeto.dir/SearchUC.cpp.o"
+  "CMakeFiles/projeto.dir/SearchUC.cpp.o.d"
+  "CMakeFiles/projeto.dir/Student_class.cpp.o"
+  "CMakeFiles/projeto.dir/Student_class.cpp.o.d"
+  "CMakeFiles/projeto.dir/main.cpp.o"
+  "CMakeFiles/projeto.dir/main.cpp.o.d"
+  "projeto"
+  "projeto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
